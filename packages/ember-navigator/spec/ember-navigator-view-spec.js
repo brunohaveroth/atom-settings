37df80293eed928634d576ebe6eb05036b32f1c4
Bruno Haveroth <bruno.haveroth@gmail.com>
2018-05-10 10:20:35 -0300

    .

--- packages/ember-navigator/spec/ember-navigator-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import EmberNavigatorView from '../lib/ember-navigator-view';
-
-describe('EmberNavigatorView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
